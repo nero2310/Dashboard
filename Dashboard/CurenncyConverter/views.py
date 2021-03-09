b4fe50007f55645befceab23d7d347a0743082e8
nero2310 <nero2310@wp.pl>
2021-03-09 23:17:03 +0100

    Bugfix: Fix typo in variable name

--- Dashboard/CurenncyConverter/views.py
+++ Dashboard/CurenncyConverter/views.py
@@ -7,5 +7,5 @@ import requests
 
 def index(request, base: str):
     response = requests.get(f'https://api.exchangeratesapi.io/latest?base={base.upper()}')
-    curenncy_data = response.json()
-    return render(request, "currency.html", context=curenncy_data)
+    currency_data = response.json()
+    return render(request, "currency.html", context=currency_data)
