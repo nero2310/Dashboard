c430cc2956ba333e7d9ed3796cb8ed3fa0fc99c9
nero2310 <nero2310@wp.pl>
2021-03-18 00:02:32 +0100

    Add error page
    Error page will appear if currency doesn't exist
    or server is not responding.

--- Dashboard/CurenncyConverter/views.py
+++ Dashboard/CurenncyConverter/views.py
@@ -7,5 +7,18 @@ import requests
 
 def index(request, base: str):
     response = requests.get(f'https://api.exchangeratesapi.io/latest?base={base.upper()}')
-    currency_data = response.json()
-    return render(request, "currency.html", context=currency_data)
+    if response:
+        currency_data = response.json()
+        return render(request, "currency.html", context=currency_data)
+    elif response.status_code == 400:
+        error_data = {
+            'Status_Code': response.status_code,
+            'Message': 'Check if the currency selected is correct'
+        }
+        return render(request, "api-error.html", context=error_data)
+    else:
+        error_data = {
+            'Status_Code': response.status_code,
+            'Message': 'Unexpected error'
+        }
+        return render(request, "api-error.html", context=error_data)
